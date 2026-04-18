b8e4a22c5b277f318c5fa4d63ef2dacd27bd0816
GylkaRoman <gylkaroman1.gmail.com>
2026-04-18 22:39:02 +0300

    feat(movies): add request validation

---
+++ laravel/app/Http/Requests/StoreMovieRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+        return [
+            'title' => 'required|string|max:100',
+            'original_title' => 'required|string|max:100',
+            'description' => 'required|string|max:500',
+            'poster_url' => 'required|url',
+            'trailer_url' => 'required|url',
+            'genre' => 'required|string|max:255',
+            'duration' => 'required|integer|min:1|max:20',
+            'age_rating' => 'required|string|max:50',
+            'director' => 'required|string|max:50',
+            'actors' => 'required|string|max:255',
+        ];
+    }
+}
