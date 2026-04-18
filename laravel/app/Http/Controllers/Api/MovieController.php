c0bdf9d91815efa78ab1976b662656b68bdb2a62
GylkaRoman <gylkaroman1.gmail.com>
2026-04-18 23:02:58 +0300

    feat(movies): add movie API controller

---
+++ laravel/app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMovieRequest;
+use App\DTO\MovieDTO;
+use App\Services\Interfaces\MovieServiceInterface;
+
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function __construct(
+        private MovieServiceInterface $service
+    ) {}
+
+    public function store(StoreMovieRequest $request) {
+        $dto = MovieDTO::fromArray($request->validated());
+
+        return $this->service->create($dto);
+    }
+}
