3cca4a59e0f50e3ed2c8024d170845565f273434
GylkaRoman <gylkaroman1.gmail.com>
2026-04-19 11:55:37 +0300

    chore(AppServiceProvider): bind movie dependencies

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,21 +2,21 @@
 
 namespace App\Providers;
 
+use App\Repositories\Impl\MovieRepository;
+use App\Repositories\Interfaces\MovieRepositoryInterface;
+use App\Services\Impl\MovieService;
+use App\Services\Interfaces\MovieServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    
     public function register(): void
     {
-        //
+        $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
+        $this->app->bind(MovieServiceInterface::class, MovieService::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
