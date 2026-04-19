820c2c7f68b943c763ac7bf03efe9eb9d59bddbd
GylkaRoman <gylkaroman1.gmail.com>
2026-04-19 11:44:29 +0300

    feat(api): add movies route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('user/main');
 });
+
+Route::post('/movies', [MovieController::class, 'store']);
