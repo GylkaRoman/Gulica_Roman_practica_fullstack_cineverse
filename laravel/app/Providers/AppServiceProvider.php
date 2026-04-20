aa07c828f3641e8efdcfc5c30b8a7044a45e7174
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 20:27:25 +0300

    feat(provider) bind hall repository and service dependencies

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Repositories\Impl\HallRepository;
 use App\Repositories\Impl\MovieRepository;
+use App\Repositories\Interfaces\HallRepositoryInterface;
 use App\Repositories\Interfaces\MovieRepositoryInterface;
+use App\Services\Impl\HallService;
 use App\Services\Impl\MovieService;
+use App\Services\Interfaces\HallServiceInterface;
 use App\Services\Interfaces\MovieServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
         $this->app->bind(MovieServiceInterface::class, MovieService::class);
+        $this->app->bind(HallRepositoryInterface::class, HallRepository::class);
+        $this->app->bind(HallServiceInterface::class, HallService::class);
     }
 
     public function boot(): void
