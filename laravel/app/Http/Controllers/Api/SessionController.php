100e86311e32eccadf2561f8eeb93294d607d2da
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 22:26:32 +0300

    feat(sessions): add SessionController

---
+++ laravel/app/Http/Controllers/Api/SessionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\SessionDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSessionRequest;
+use App\Services\Interfaces\SessionServiceInterface;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function __construct(
+        private SessionServiceInterface $service
+    ) {}
+
+    public function store(StoreSessionRequest $request) {
+        $dto = SessionDTO::fromArray($request->validated());
+
+        return $this->service->create($dto);
+    }
+
+    public function index(Request $request)
+    {
+        $perPage = $request->query('per_page', 10);
+        return $this->service->getAll($perPage);
+    }
+}
