1512842800ab0b4c49a4534c52ed8298f93ec56c
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 12:43:47 +0300

    feat(routes/api.php): add GET /movies endpoint and delete unused default route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
 
-Route::get('/', function () {
-    return view('user/main');
-});
-
 Route::post('/movies', [MovieController::class, 'store']);
+
+Route::get('/movies', [MovieController::class, 'index']);
