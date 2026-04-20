79975a30d2ba0b57a80c6f30463974ad92f21c53
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 18:39:22 +0300

    refactor(Hall): add array fillable

--- laravel/app/Models/Hall.php
+++ laravel/app/Models/Hall.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hall extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'rows_count',
+        'seats_per_row',
+    ];
 }
