91aa60429f49f9c133a18ea11343305229cbdf48
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 21:33:51 +0300

    feat(session): create sessions table migration

---
+++ laravel/database/migrations/2026_04_20_182351_create_sessions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('hall_id')->constrained()->cascadeOnDelete();
+
+            $table->date('date');
+            $table->time('time');
+
+            $table->string('format');
+            $table->string('language');
+
+            $table->decimal('base_price', 8, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
