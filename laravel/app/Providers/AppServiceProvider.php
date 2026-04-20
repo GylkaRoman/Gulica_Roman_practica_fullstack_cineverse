7f0d00e8a0a64f27e98431a7688eafa2899ecfd0
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 22:31:16 +0300

    feat(sessions): bind session interfaces in service provider

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -4,12 +4,16 @@ namespace App\Providers;
 
 use App\Repositories\Impl\HallRepository;
 use App\Repositories\Impl\MovieRepository;
+use App\Repositories\Impl\SessionRepository;
 use App\Repositories\Interfaces\HallRepositoryInterface;
 use App\Repositories\Interfaces\MovieRepositoryInterface;
+use App\Repositories\Interfaces\SessionRepositoryInterface;
 use App\Services\Impl\HallService;
 use App\Services\Impl\MovieService;
+use App\Services\Impl\SessionService;
 use App\Services\Interfaces\HallServiceInterface;
 use App\Services\Interfaces\MovieServiceInterface;
+use App\Services\Interfaces\SessionServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(MovieServiceInterface::class, MovieService::class);
         $this->app->bind(HallRepositoryInterface::class, HallRepository::class);
         $this->app->bind(HallServiceInterface::class, HallService::class);
+        $this->app->bind(SessionRepositoryInterface::class, SessionRepository::class);
+        $this->app->bind(SessionServiceInterface::class, SessionService::class);
     }
 
     public function boot(): void
