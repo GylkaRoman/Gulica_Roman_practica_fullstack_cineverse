ba19e95eca4d69287ca9a1c66045a8719c7d5edb
GylkaRoman <gylkaroman1.gmail.com>
2026-04-20 22:33:45 +0300

    feat(sessions): add GET/POST API routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\HallController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\SessionController;
 
 Route::post('/movies', [MovieController::class, 'store']);
 
@@ -13,3 +14,7 @@ Route::get('/movies/{id}', [MovieController::class, 'show']);
 Route::post('/halls', [HallController::class, 'store']);
 
 Route::get('/halls', [HallController::class, 'index']);
+
+Route::post('/sessions', [SessionController::class, 'store']);
+
+Route::get('/sessions', [SessionController::class, 'index']);
