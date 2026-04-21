f2b363544e5e5284c50cb3a1abbea8a1c9e47262
GylkaRoman <gylkaroman1.gmail.com>
2026-04-21 20:32:32 +0300

    feat(DatabaseSeeder): rigister SessionSeeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MovieSeeder::class,
             HallSeeder::class,
+            SessionSeeder::class,
         ]);
     }
 }
