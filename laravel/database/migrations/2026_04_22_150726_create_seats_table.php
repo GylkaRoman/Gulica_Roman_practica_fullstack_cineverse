7cc072bcdb545f2a1962fd77533a3fe84eadba7e
GylkaRoman <gylkaroman1.gmail.com>
2026-04-22 18:17:20 +0300

    feat(migration): add new migration for table seats

---
+++ laravel/database/migrations/2026_04_22_150726_create_seats_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hall_id')->constrained()->cascadeOnDelete();
+
+            $table->integer('row');
+            $table->integer('number');
+
+            $table->enum('type', ['standart', 'vip'])->default('standart');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
