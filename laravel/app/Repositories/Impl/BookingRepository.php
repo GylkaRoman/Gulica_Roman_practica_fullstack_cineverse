f36e0fbc8f5011522c3415e796060e0705f94976
GylkaRoman <gylkaroman1.gmail.com>
2026-04-23 12:52:41 +0300

    feat(bookingRepository): add new method getBookedIds

--- laravel/app/Repositories/Impl/BookingRepository.php
+++ laravel/app/Repositories/Impl/BookingRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Impl;
 
 use App\Models\Booking;
 use App\Repositories\Interfaces\BookingRepositoryInterface;
+use Illuminate\Support\Facades\DB;
 
 class BookingRepository implements BookingRepositoryInterface
 {
@@ -11,5 +12,15 @@ class BookingRepository implements BookingRepositoryInterface
     public function create(array $data)
     {
         return Booking::create($data);
+    }
+
+    public function getBookedSeatIds(int $sessionId, array $seatIds)
+    {
+        return DB::table('booking_seat')
+            ->join('bookings', 'booking_seat.booking_id', '=', 'bookings.id')
+            ->where('bookings.session_id', $sessionId)
+            ->whereIn('booking_seat.seat_id', $seatIds)
+            ->pluck('seat_id')
+            ->toArray();
     }
 }
--- laravel/app/Repositories/Interfaces/BookingRepositoryInterface.php
+++ laravel/app/Repositories/Interfaces/BookingRepositoryInterface.php
@@ -5,4 +5,6 @@ namespace App\Repositories\Interfaces;
 interface BookingRepositoryInterface
 {
     public function create(array $data);
+
+    public function getBookedSeatIds(int $sessionId, array $seatIds);
 }
