3b7993468ab94c94238d7e1190e82800fe3c6132
GylkaRoman <gylkaroman1.gmail.com>
2026-05-08 18:36:43 +0300

    fix(Booking): fix booking problem and now is simple method of multiply booking

--- laravel/app/Http/Requests/StoreBookingRequest.php
+++ laravel/app/Http/Requests/StoreBookingRequest.php
@@ -15,9 +15,13 @@ class StoreBookingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'session_id' => ['required', 'exists:movie_sessions,id'],
-            'seat_ids' => ['required', 'array', 'min:1'],
-            'seat_ids.*' => ['exists:seats,id'],
-        ];
+        'session_id' => ['required', 'exists:movie_sessions,id'],
+
+        'seat_ids' => ['required', 'array', 'min:1'],
+        'seat_ids.*' => ['exists:seats,id'],
+
+        'ticket_type' => ['required', 'in:standard,student,child'],
+        'format' => ['required', 'in:2D,3D'],
+    ];
     }
 }
